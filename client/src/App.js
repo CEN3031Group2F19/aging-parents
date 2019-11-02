7824471f6d46fc814945303f8c6fbb6ebe48efec
Brandon Correa <50252804+bwancor@users.noreply.github.com>
2019-11-02 14:12:00 -0400

    Add Route to Notes Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
+
+/* For new pages:
+  import PageName from "./PagePath"
+*/
 import Home from "./views/Home/Home"
+import Notes from "./views/Notes/Notes"
 import NotFound from "./views/NotFound"
 import Header from "./components/Header/Header"
-
 
 const App = () => {
   return (
     <div>
       <Header />
       <Switch>
+        {/* For new routes:
+          <Route exact path="/YourPathName" component={PageName}
+        */}
         <Route exact path="/Home" component={Home} />
+        <Route exact path="/Notes" component={Notes} />
+
         <Route exact path="/">
           <Redirect to="/Home" />
         </Route>
