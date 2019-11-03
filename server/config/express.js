5bda474f98bfbebdec0a8a4297afafddaab4187f
Regan Smith <smith.regan.d@gmail.com>
2019-11-03 11:50:04 -0500

    Begins work on authenticating users via passport

--- server/config/express.js
+++ server/config/express.js
@@ -1,43 +1,85 @@
-const path = require('path'),
-    express = require('express'),
-    mongoose = require('mongoose'),
-    morgan = require('morgan'),
-    bodyParser = require('body-parser'),
-    exampleRouter = require('../routes/examples.server.routes');
+const path = require("path"),
+  express = require("express"),
+  mongoose = require("mongoose"),
+  morgan = require("morgan"),
+  bodyParser = require("body-parser"),
+  exampleRouter = require("../routes/examples.server.routes"),
+  users = require("../routes/authentication.routes"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local").Strategy;
+var Account = require("../models/account");
 
 module.exports.init = () => {
-    /* 
+  /* 
         connect to database
         - reference README for db uri
     */
-    mongoose.connect(process.env.DB_URI || require('./config').db.uri, {
-        useNewUrlParser: true
+  mongoose.connect(process.env.DB_URI || require("./config").db.uri, {
+    useNewUrlParser: true
+  });
+  mongoose.set("useCreateIndex", true);
+  mongoose.set("useFindAndModify", false);
+
+  // initialize app
+  const app = express();
+
+  // enable request logging for development debugging
+  app.use(morgan("dev"));
+
+  // body parsing middleware
+  app.use(bodyParser.json());
+
+  //user authentication middleware
+  app.use(passport.initialize());
+  app.use(passport.session());
+  passport.use(new LocalStrategy(Account.authenticate()));
+  passport.serializeUser(Account.serializeUser());
+  passport.deserializeUser(Account.deserializeUser());
+
+  // add a router
+  app.use("/api/example", exampleRouter);
+  app.use("/", users);
+
+  if (process.env.NODE_ENV === "production") {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, "../../client/build")));
+
+    // Handle React routing, return all requests to React app
+    app.get("*", function(req, res) {
+      res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
     });
-    mongoose.set('useCreateIndex', true);
-    mongoose.set('useFindAndModify', false);
+  }
 
-    // initialize app
-    const app = express();
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+  });
 
-    // enable request logging for development debugging
-    app.use(morgan('dev'));
+  // error handlers
 
-    // body parsing middleware
-    app.use(bodyParser.json());
+  // development error handler
+  // will print stacktrace
+  if (app.get("env") === "development") {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render("error", {
+        message: err.message,
+        error: err
+      });
+    });
+  }
 
-    // add a router
-    app.use('/api/example', exampleRouter);
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: {}
+    });
+  });
 
-    if (process.env.NODE_ENV === 'production') {
-        // Serve any static files
-        app.use(express.static(path.join(__dirname, '../../client/build')));
-
-        // Handle React routing, return all requests to React app
-        app.get('*', function(req, res) {
-            res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
-        });
-    }
-
-    return app
-}
-
+  return app;
+};
