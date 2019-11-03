a98c911286213b102787f59f812e61ca61024608
Regan Smith <smith.regan.d@gmail.com>
2019-11-03 17:14:57 -0500

    First pass at authenticating users
    The JWT is stored as state in App and will be used to authenticate all requests

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,44 @@
-import React from 'react';
-import { Route, Switch, Redirect  } from 'react-router-dom';
-import Home from "./views/Home/Home"
-import NotFound from "./views/NotFound"
-import Header from "./components/Header/Header"
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Home from "./views/Home/Home";
+import NotFound from "./views/NotFound";
+import Header from "./components/Header/Header";
+import Login from "./components/Login";
 
-
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/Home" component={Home} />
-        <Route exact path="/">
-          <Redirect to="/Home" />
-        </Route>
-        <Route component={NotFound}/>
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: ""
+    };
+  }
+  authenticateUser(token) {
+    this.setState({ token: token });
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route
+            exact
+            path="/login"
+            render={props => (
+              <Login
+                {...props}
+                authenticateUser={this.authenticateUser.bind(this)}
+              />
+            )}
+          />
+          <Route exact path="/Home" component={Home} />
+          <Route exact path="/">
+            <Redirect to="/Home" />
+          </Route>
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
