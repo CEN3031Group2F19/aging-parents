882e65296c92b8c8b691bfa0247134371b52d1a7
Kevin Scott <kev8144@gmail.com>
2019-11-12 13:36:06 -0500

    feat: Add dateTime package to use with  tracking of tasks

--- client/src/views/DailyTasks/TodoList.js
+++ client/src/views/DailyTasks/TodoList.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
+import Moment from 'react-moment';
+
 
 class TodoList extends Component {
   componentDidUpdate() {
     this.props.inputElement.current.focus()
+    
   }
+ // let dateToFormat = '1976-04-19T12:59-0500';
+
   render() {
     return (
       <div>
+        <Moment></Moment>
         <div>
           <form onSubmit={this.props.addItem}>
             <input
@@ -14,8 +20,7 @@ class TodoList extends Component {
               ref={this.props.inputElement}
               value={this.props.currentItem.text}
               onChange={this.props.handleInput}
-            />
-            
+            />           
             <div>
             <button type="submit"> Add Task </button>
             <button type="submit"> Export </button>
