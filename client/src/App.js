48471c914f0c7ad5744c8a4b9aa7745695d7027a
Brandon Correa <50252804+bwancor@users.noreply.github.com>
2019-11-11 20:10:33 -0500

    Change Notes Path in Import

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "./views/Home/Home";
-import Notes from "./views/Notes/Notes";
+import Notes from "./components/Notes/Notes";
 import NotFound from "./views/NotFound";
 import Header from "./components/Header/Header";
 import Login from "./components/Login";
@@ -41,34 +41,6 @@ class App extends React.Component {
       </div>
     );
   }
-  // import React from 'react';
-  // import { Route, Switch, Redirect  } from 'react-router-dom';
-
-  // /* For new pages:
-  //   import PageName from "./PagePath"
-  // */
-  // import Home from "./views/Home/Home"
-  // import Notes from "./views/Notes/Notes"
-  // import NotFound from "./views/NotFound"
-  // import Header from "./components/Header/Header"
-
-  // const App = () => {
-  //   return (
-  //     <div>
-  //       <Header />
-  //       <Switch>
-  //         {/* For new routes:
-  //           <Route exact path="/YourPathName" component={PageName}
-  //         */}
-  //         <Route exact path="/Home" component={Home} />
-
-  //         <Route exact path="/">
-  //           <Redirect to="/Home" />
-  //         </Route>
-  //         <Route component={NotFound}/>
-  //       </Switch>
-  //     </div>
-  //   );
 }
 
 export default App;
