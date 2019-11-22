c3ed8e24ca91698876861fb684f5919ec6dcf25b
Brandon Correa <50252804+bwancor@users.noreply.github.com>
2019-11-21 22:17:12 -0500

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,33 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "./views/Home/Home";
+
+
+import DailyTaskList from "./views/DailyTaskList/DailyTaskList";
+import DailyTaskItems from "./views/DailyTaskItems/DailyTaskItems";
 import Notes from "./components/Notes/Notes";
 
-import DailyTasks from "./views/DailyTasks/DailyTasks";
+import AppointmentView from './views/CalendarView/AppointmentView'
+import CalendarView from './views/CalendarView/CalendarView'
+import CalendarDateView from './views/CalendarView/CalendarDateView'
+import Timesheet from "./views/Timesheet/Timesheet";
 import NotFound from "./views/NotFound";
 import SignUp from "./views/SignUp/SignUp";
 import Header from "./components/Header/Header";
 import Login from "./components/Login";
 
 class App extends React.Component {
+  inputElement = React.createRef();
   constructor(props) {
     super(props);
     const token = localStorage.getItem("token");
     this.state = {
-      token
+      token,
+      items: [],
+      currentItem: {
+        text: '',
+        key: '',
+      },
     };
   }
 
@@ -32,6 +45,40 @@ class App extends React.Component {
     localStorage.removeItem("token");
     this.setState({ token: null });
   }
+
+  // Daily tasks functionality:
+
+  // delete task
+  deleteItem = key => {
+    const filteredItems = this.state.items.filter(item => {
+      return item.key !== key
+    })
+    this.setState({
+      items: filteredItems,
+    })
+  }
+
+  handleInput = e => {
+    const itemText = e.target.value
+    const currentItem = { text: itemText, key: Date.now() }
+    this.setState({
+      currentItem,
+    })
+  }
+
+  //Add task
+  addItem = e => {
+    e.preventDefault()
+    const newItem = this.state.currentItem
+    if (newItem.text !== '') {
+      const items = [...this.state.items, newItem]
+      this.setState({
+        items: items,
+        currentItem: { text: '', key: '' },
+      })
+    }
+  }
+
   render() {
     return (
       <div>
@@ -66,12 +113,71 @@ class App extends React.Component {
             path="/DailyTasks"
             render={props =>
               this.isUserSignedIn() ? (
-                <DailyTasks {...props} />
+               [ <DailyTaskList
+                addItem={this.addItem}
+                inputElement={this.inputElement}
+                handleInput={this.handleInput}
+                currentItem={this.state.currentItem} />,                 <
+                  DailyTaskItems entries={this.state.items} deleteItem={this.deleteItem} /> 
+              ]
               ) : (
                 <Redirect to="/Home" />
               )
             }
-          />          
+          />
+          <Route
+            exact
+            path="/Calendar"
+            render={props =>
+              this.isUserSignedIn() ? (
+                <CalendarView
+                  year={new Date().getFullYear()} 
+                  day={new Date().getDate()}
+                  month={new Date().getMonth()} /> 
+              ) : (
+                <Redirect to="/Home" />
+              )
+            }
+          />
+          <Route
+          exact
+          path="/Calendar/Date/:year/:month/:day"
+          render={props =>
+            this.isUserSignedIn() ? (
+              <CalendarDateView
+                year={props.match.params.year} 
+                day={props.match.params.day}
+                month={props.match.params.month - 1} />
+            ) : (
+              <Redirect to="/Calendar" />
+            )
+          }
+        />
+        <Route
+        exact
+        path="/Calendar/Appointment/:year/:month/:day"
+        render={props =>
+          this.isUserSignedIn() ? (
+            <AppointmentView
+              year={props.match.params.year} 
+              day={props.match.params.day}
+              month={props.match.params.month - 1} />
+          ) : (
+            <Redirect to="/Calendar" />
+          )
+        }
+      />
+          <Route
+            exact
+            path="/Timesheet"
+            render={props =>
+              this.isUserSignedIn() ? (
+                <Timesheet {...props} />
+              ) : (
+                <Redirect to="/Home" />
+              )
+            }
+          />                
           <Route exact path="/Home" component={Home} />
           <Route
             exact
