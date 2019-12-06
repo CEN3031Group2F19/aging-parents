409ce38f02379658d00f8588e906956078bd72e2
Regan Smith <smith.regan.d@gmail.com>
2019-12-05 21:05:24 -0500

    Front and back end flows for resetting passwords

---
+++ client/src/views/ForgotPassword.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { Form, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+const axios = require("axios");
+
+class ForgotPassword extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: "",
+      showError: false,
+      messageFromServer: "",
+      showNullError: false
+    };
+  }
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
+
+  sendEmail = async e => {
+    e.preventDefault();
+    const { email } = this.state;
+    if (email === "") {
+      this.setState({
+        showError: false,
+        messageFromServer: "",
+        showNullError: true
+      });
+    } else {
+      try {
+        const serverUri = "http://localhost:5000";
+        const response = await axios.post(`${serverUri}/forgotPassword`, {
+          email
+        });
+        console.log(response.data);
+        if (response.data === "recovery email sent") {
+          this.setState({
+            showError: false,
+            messageFromServer: "recovery email sent",
+            showNullError: false
+          });
+        }
+      } catch (error) {
+        console.error(error.response.data);
+        if (error.response.data === "email not in db") {
+          this.setState({
+            showError: true,
+            messageFromServer: "",
+            showNullError: false
+          });
+        }
+      }
+    }
+  };
+
+  render() {
+    const { email, messageFromServer, showNullError, showError } = this.state;
+    return (
+      <div className="forgotpassword">
+        <h1>Forgot Password</h1>
+        <Form>
+          {/* <Form.Group widths="equal"> */}
+          <Form.Input
+            fluid
+            label="Email"
+            placeholder="example@example.com"
+            onChange={(e, v) => this.setState({ email: v.value })}
+          />{" "}
+          <Form.Field control={Button} onClick={this.sendEmail}>
+            Submit
+          </Form.Field>
+        </Form>
+        {showNullError && (
+          <div>
+            <p>The email address cannot be null.</p>
+          </div>
+        )}
+        {showError && (
+          <div>
+            <p>
+              That email address isn&apos;t recognized. Please try again or
+              register for a new account.
+            </p>
+            <Link to="/signup">Create an account</Link>
+          </div>
+        )}
+        {messageFromServer === "recovery email sent" && (
+          <div>
+            <h3>Password Reset Email Successfully Sent!</h3>
+          </div>
+        )}
+        <Link to="/home">Go home</Link>
+      </div>
+    );
+  }
+}
+export default ForgotPassword;
--- server/routes/authentication.routes.js
+++ server/routes/authentication.routes.js
@@ -3,6 +3,8 @@ var passport = require("passport");
 var Account = require("../models/account");
 var router = express.Router();
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const crypto = require("crypto");
 
 router.get("/", function(req, res) {
   passport.authenticate("jwt", { session: false })(req, res, function() {
@@ -42,8 +44,129 @@ router.get("/logout", function(req, res) {
   res.redirect("/");
 });
 
-router.get("/ping", function(req, res) {
-  res.status(200).send("pong!");
+// router.get("/reset", (req, res) => {
+//   Account.findOne({
+//     //username: req.body.username,
+//     resetPasswordToken: req.body.resetPasswordToken
+//     //resetPasswordExpires: { $gte: new Date() }
+//   }).then(user => {
+//     if (user == null) {
+//       console.error("Reset: password reset link is invalid or has expired");
+//       res.status(403).send("password reset link is invalid or has expired");
+//     } else {
+//       console.log("username: ", user.username);
+//       res.status(200).send({
+//         username: user.username,
+//         message: "password reset link a-ok"
+//       });
+//     }
+//   });
+// });
+router.get("/reset", (req, res) => {
+  console.log("TOKEN: ", req.query.resetPasswordToken);
+  Account.findOne(
+    {
+      //username: req.body.username,
+      resetPasswordToken: req.query.resetPasswordToken
+      //resetPasswordExpires: { $gte: new Date() }
+    },
+    (err, user) => {
+      if (user == null) {
+        console.error("Reset: password reset link is invalid or has expired");
+        res.status(403).send("password reset link is invalid or has expired");
+      } else {
+        console.log("username: ", user.username);
+        res.status(200).send({
+          username: user.username,
+          message: "password reset link a-ok"
+        });
+      }
+    }
+  );
+});
+
+router.put("/updatePasswordViaEmail", (req, res) => {
+  console.log(req.body.username);
+  Account.findOne({
+    username: req.body.username,
+    resetPasswordToken: req.body.resetPasswordToken
+    //todo: compare dates
+    // resetPasswordExpires: {
+    //   $gte: Date.now()
+    // }
+  }).then(user => {
+    if (user == null) {
+      console.error("password reset link is invalid or has expired");
+      res.status(403).send("password reset link is invalid or has expired");
+    } else if (user != null) {
+      console.log("user exists in db");
+      console.log(req.body.password);
+      user.setPassword(req.body.password, () => user.save());
+      // user.resetPasswordToken = null;
+      // user.resetPasswordExpires = null;
+      user.save().then(() => {
+        console.log("password updated");
+        res.status(200).send({ message: "password updated" });
+      });
+    } else {
+      console.error("no user exists in db to update");
+      res.status(401).json("no user exists in db to update");
+    }
+  });
+});
+
+router.post("/forgotpassword", (req, res) => {
+  if (req.body.email === "") {
+    res.status(400).send("email required");
+  }
+  console.error(req.body.email);
+  Account.findOne({
+    username: req.body.email
+  }).then(acct => {
+    console.log(acct);
+    if (acct === undefined) {
+      console.error("email not in database");
+      res.status(403).send("email not in db");
+    } else {
+      const token = crypto.randomBytes(20).toString("hex");
+      acct.resetPasswordToken = token;
+      acct.resetPasswordExpires = Date.now() + 360000;
+      acct.save();
+      console.log(
+        require("../config/config").gmail.email,
+        require("../config/config").gmail.password
+      );
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: `${require("../config/config").gmail.email}`,
+          pass: `${require("../config/config").gmail.password}`
+        }
+      });
+
+      const mailOptions = {
+        from: "testPatientCare@gmail.com",
+        to: `${acct.username}`,
+        subject: "Link To Reset Password",
+        text:
+          "You are receiving this because you have requested a password reset.\n\n" +
+          "Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n" +
+          `localhost:3000/resetPassword/${token}\n\n` +
+          "If you did not request this, please ignore this email and your password will remain unchanged.\n"
+      };
+
+      console.log("sending mail");
+
+      transporter.sendMail(mailOptions, (err, response) => {
+        if (err) {
+          console.error("there was an error: ", err);
+        } else {
+          console.log("here is the res: ", response);
+          res.status(200).json("recovery email sent");
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
